815f6471a77a4d57e32f60ad032e6372afafb2d6
Weiyu Liu <wliu88@gatech.edu>
2023-01-20 18:04:42 -0800

    remove extra code

--- src/StructDiffusion/data/MeshSceneViewer.py
+++ src/StructDiffusion/data/MeshSceneViewer.py
@@ -22,19 +22,6 @@ class MeshSceneViewer:
                 # TODO: can also enforce scene or single mesh here
                 self.urdf_to_mesh[urdf] = trimesh.load(os.path.join(self.assets_dir, mesh_type, filename))
 
-    # def _get_ids(self, h5):
-    #     """
-    #     get object ids
-    #
-    #     @param h5:
-    #     @return:
-    #     """
-    #     ids = {}
-    #     for k in h5.keys():
-    #         if k.startswith("id_"):
-    #             ids[k[3:]] = h5[k][()]
-    #     return ids
-
     def load_object_mesh_from_urdf(self, urdf):
         if self.cache_mesh:
             return self.urdf_to_mesh[urdf[:-5]].copy()
@@ -42,67 +29,6 @@ class MeshSceneViewer:
             mesh_path = os.path.join(self.assets_dir, self.mesh_type, urdf[:-5] + ".obj")
             mesh = trimesh.load(mesh_path)
             return mesh
-
-    def process_file(self, filename):
-
-        print(filename)
-
-        h5 = h5py.File(filename, 'r')
-        ids = self._get_ids(h5)
-        all_objs = sorted([o for o in ids.keys() if "object_" in o])
-
-        if "goal_specification" not in h5:
-            print("skipping this file because goal_specification is not in h5")
-            return
-
-        goal_specification = json.loads(str(np.array(h5["goal_specification"])))
-        num_rearrange_objs = len(goal_specification["rearrange"]["objects"])
-
-        target_objs = all_objs[:num_rearrange_objs]
-
-        structure_parameters = goal_specification["shape"]
-
-        target_obj_urdfs = [obj_spec["urdf"] for obj_spec in goal_specification["rearrange"]["objects"]]
-
-        # Important: ensure the order is correct
-        if structure_parameters["type"] == "circle" or structure_parameters["type"] == "line":
-            target_objs = target_objs[::-1]
-            target_obj_urdfs = target_obj_urdfs[::-1]
-        elif structure_parameters["type"] == "tower" or structure_parameters["type"] == "dinner":
-            target_objs = target_objs
-            target_obj_urdfs = target_obj_urdfs
-        else:
-            raise KeyError("{} structure is not recognized".format(structure_parameters["type"]))
-
-        target_obj_vis_meshes = [self.load_object_mesh_from_object_info(u) for u in target_obj_urdfs]
-        initial_obj_vis_meshes = []
-        goal_obj_vis_meshes = []
-        for obj, obj_vis_mesh in zip(target_objs, target_obj_vis_meshes):
-            initial_pose = h5[obj][0]
-            goal_pose = h5[obj][-1]
-
-            initial_obj_vis_mesh = obj_vis_mesh.copy()
-            initial_obj_vis_mesh.apply_transform(initial_pose)
-            initial_obj_vis_meshes.append(initial_obj_vis_mesh)
-
-            goal_obj_vis_mesh = obj_vis_mesh.copy()
-            goal_obj_vis_mesh.apply_transform(goal_pose)
-            goal_obj_vis_meshes.append(goal_obj_vis_mesh)
-
-        scene = trimesh.Scene()
-        # add the coordinate frame first
-        geom = trimesh.creation.axis(0.01)
-        scene.add_geometry(geom)
-        table = trimesh.creation.box(extents=[1.0, 1.0, 0.02])
-        table.apply_translation([0.5, 0, -0.01])
-        table.visual.vertex_colors = [0, 255, 0, 125]
-        scene.add_geometry(table)
-
-        scene.add_geometry(goal_obj_vis_meshes)
-        scene.show()
-
-        # trimesh.Scene(initial_obj_vis_meshes).show()
-        # trimesh.Scene(goal_obj_vis_meshes).show()
 
     def load_mesh_scene(self, target_objs, goal_specification, current_obj_poses, current_pc_poses, goal_pc_poses, visualize=False):
 
@@ -117,19 +43,6 @@ class MeshSceneViewer:
             raise KeyError("{} structure is not recognized".format(structure_parameters["type"]))
 
         target_obj_meshes = [self.load_object_mesh_from_urdf(u) for u in target_obj_urdfs]
-
-        # goal_obj_vis_meshes = []
-        # for obj, obj_vis_mesh in zip(target_objs, target_obj_vis_meshes):
-        #     # initial_pose = h5[obj][0]
-        #     # goal_pose = h5[obj][-1]
-        #
-        #     initial_obj_vis_mesh = obj_vis_mesh.copy()
-        #     initial_obj_vis_mesh.apply_transform(initial_pose)
-        #     initial_obj_vis_meshes.append(initial_obj_vis_mesh)
-        #
-        #     goal_obj_vis_mesh = obj_vis_mesh.copy()
-        #     goal_obj_vis_mesh.apply_transform(goal_pose)
-        #     goal_obj_vis_meshes.append(goal_obj_vis_mesh)
 
         for i, obj_name in enumerate(target_objs):
             current_obj_pose = current_obj_poses[i]
